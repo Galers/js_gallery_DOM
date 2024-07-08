177088b0aba873f640bbf2dff5560b5467ab5cb2
Galers <galosaandrew@gmail.com>
2024-07-08 15:39:41 +0300

    make build

---
+++ index.a6604f01.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.querySelector(".gallery").addEventListener("click",function(t){if(t.target.classList.contains("gallery__thumb")){t.preventDefault();var r=t.target.closest(".list-item__link").getAttribute("href");e.src=r}});
+//# sourceMappingURL=index.a6604f01.js.map
